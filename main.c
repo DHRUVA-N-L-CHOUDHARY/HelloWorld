982cc5f92157c47adb35b89085172a4c57ea8491
DHRUVA-N-L-CHOUDHARY <96297602+DHRUVA-N-L-CHOUDHARY@users.noreply.github.com>
2022-03-20 13:26:40 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,66 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+unsigned long long int i = 0;
+unsigned long long int j = 0;
+unsigned long long int k = 0;
+unsigned long long int m = 0;
+unsigned long long int n = 0;
+unsigned long long int o = 0;
+unsigned long long int p = 0;
+unsigned long long int q = 0;
+unsigned long long int r = 0;
+unsigned long long int s = 0;
+int
+main ()
+{
+  while (i > 0)
+    {
+      i = i - 1;
+      while (j > 0)
+	{
+	  j = j - 1;
+	  while (k > 0)
+	    {
+	      k = k - 1;
+	      while (m > 0)
+		{
+		  m = m - 1;
+		  while (n > 0)
+		    {
+		      n = n - 1;
+		      while (o > 0)
+			{
+			  o = o - 1;
+			  while (p > 0)
+			    {
+			      p = p - 1;
+			      while (q > 0)
+				{
+				  q = q - 1;
+				  while (r > 0)
+				    {
+				      r = r - 1;
+				      while (s > 0)
+					{
+					  s = s - 1;
+					}
+				    }
+				}
+			    }
+			}
+		    }
+		}
+	    }
+	}
+    }
+  printf ("Hello World");
+
+  return 0;
+}
